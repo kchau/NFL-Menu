7111d80b30ba66960b259e3a1dabb7b3ef429d89
Kafu Chau <kchau@lgscout.com>
2013-08-08 21:30:13 -0400

    Show active game stats

--- NFL-Menu/SYNGameViewController.m
+++ NFL-Menu/SYNGameViewController.m
@@ -6,6 +6,28 @@
 //  Updated by Kafu Chau on 8/7/13.
 //  Copyright (c) 2013 Syntaxi. All rights reserved.
 //
+/*
+{
+ eid: "2013080800",
+ gsis: "56095",
+ d: "Thu",
+ t: "7:30",
+ q: "2", -quarter
+ k: "07:02", -time left
+ h: "TB",
+ hnn: "buccaneers",
+ hs: "6",
+ v: "BAL",
+ vnn: "ravens",
+ vs: "7",
+ p: "TB", -possession
+ rz: "0",
+ ga: "",
+ gt: "PRE"
+}
+
+*/
+
 
 #import "SYNGameViewController.h"
 #import "SYNGameView.h"
@@ -27,21 +49,23 @@
     raw = _raw;
     [home setImage:[NSImage imageNamed:[NSString stringWithFormat:@"%@.png", raw[@"h"]]]];
     [away setImage:[NSImage imageNamed:[NSString stringWithFormat:@"%@.png", raw[@"v"]]]];
-    if([raw[@"q"] isEqualToString:@"P"]) {
+    if ([raw[@"q"] isEqualToString:@"P"]) {
         [scoreBox setSegmentCount:1];
         [scoreBox setLabel:[NSString stringWithFormat:@"%@ %@", raw[@"d"], raw[@"t"]] forSegment:0];
         [scoreBox setWidth:96 forSegment:0];
-    }else{
-        [scoreBox setLabel:raw[@"vs"] forSegment:0];
-        [scoreBox setLabel:raw[@"hs"] forSegment:2];
-        //TODO: Update this stuff once an active game is actually going...
-        if([raw[@"status"] isEqualToString:@"In Progress"]) {
-            NSString *tb = ([raw[@"top_inning"] isEqualToString:@"Y"]) ? @"▲" : @"▼";
-            [scoreBox setLabel:[NSString stringWithFormat:@"%@%@ %@", raw[@"inning"], [self ordinalFor:raw[@"inning"]], tb] forSegment:1];
-        }else if([raw[@"status"] isEqualToString:@"Final"] || [raw[@"status"] isEqualToString:@"Game Over"]) {
-             [scoreBox setLabel:@"Final" forSegment:1];
-        }else {
-            [scoreBox setLabel:[NSString stringWithFormat:@"%@", raw[@"status"]] forSegment:1];
+    } else if ([raw[@"q"] isEqualToString:@"H"]) {
+        [scoreBox setLabel:[NSString stringWithFormat:@"%@", raw[@"vs"]] forSegment:0];
+        [scoreBox setLabel:[NSString stringWithFormat:@"%@", raw[@"hs"]] forSegment:2];
+        [scoreBox setLabel:@"Halftime" forSegment:1];
+    } else {
+        [scoreBox setLabel:[NSString stringWithFormat:@"%@", raw[@"vs"]] forSegment:0];
+        [scoreBox setLabel:[NSString stringWithFormat:@"%@", raw[@"hs"]] forSegment:2];
+        [scoreBox setLabel:[NSString stringWithFormat:@"%@%@ %@", raw[@"q"], [self ordinalFor:raw[@"q"]], raw[@"k"]] forSegment:1];
+
+        if ([raw[@"p"] isEqualToString:raw[@"v"]]) {
+            [scoreBox setSelected:YES forSegment:0];
+        } else {
+            [scoreBox setSelected:YES forSegment:2];
         }
     }
 }
